1f14360a85690aaaebeae000f93186db387ac632
Daniel Lenski <dlenski@gmail.com>
2025-11-07 15:03:29 -0800

    Use new WS endpoints
    Old endpoints don't work anymore

--- engame/test_ws.py
+++ engame/test_ws.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import json
 import uuid
+import random
 from datetime import datetime
 
 from websockets.sync.client import connect
@@ -13,22 +14,46 @@ from .yq import YFQuoteResult, nav, navs
 #logging.basicConfig(level=logging.DEBUG)
 
 sess = requests.session()
-sess.headers['user-agent'] = ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
-
+sess.headers.update({
+    'User-Agent': (ua := 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'),
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+})
 
 email, password, otp = sys.argv[1:4]
 
-# This old API still works
-# https://github.com/seansullivan44/Wealthsimple-Trade-Python/blob/0b6370efb7632dd106603edcafe060d213e8f31c/src/wealthsimple/wealthsimple.py#L68-L122
-resp = sess.post('https://trade-service.wealthsimple.com/auth/login', dict(email=email, password=password, otp=otp))
+sess_uuid = uuid.uuid1()
+devid = random.getrandbits(256).to_bytes(32).hex()
+resp = sess.post(
+    'https://api.production.wealthsimple.com/v1/oauth/v2/token',
+    headers={'X-Wealthsimple-Client': '@wealthsimple/wealthsimple',
+             'X-Ws-Profile': 'undefined',
+             'X-Ws-Session-Id': str(sess_uuid),
+             'X-Ws-Device-Id': devid,                              # "Real" device ID doesn't seem necessary
+             'X-Wealthsimple-Otp': otp + ';remember=true',
+    },
+    json={
+        "grant_type": "password",
+        "username": email,
+        "password": password,
+        "skip_provision": True,
+        "otp_claim": None,
+        "scope": "invest.read invest.write trade.read trade.write tax.read tax.write",
+        "client_id": "4da53ac2b03225bed1550eba8e4611e086c7b905a3855e6ed12ea08c246758fa",  # FIXED?
+    }
+)
 assert resp.ok, resp.content
-devid, atoken, rtoken, exp = resp.headers['x-ws-device-id'], resp.headers['x-access-token'], resp.headers['x-refresh-token'], resp.headers['x-access-token-expires']
-sess.headers['authorization'] = auth = 'Bearer ' + atoken
+j = resp.json()
+assert j['token_type'] == 'Bearer'
+atoken, rtoken, exp = j['access_token'], j['refresh_token'], j['created_at'] + j['expires_in']
 
-# This one does too
-resp = sess.get('https://trade-service.wealthsimple.com/securities?query=hxs')
-import pprint
-pprint.pprint(resp.json())
+sess.headers.update(sess_info := {
+    "authorization": 'Bearer ' + atoken,
+    "x-ws-api-version": '12',
+    "x-ws-locale": "en-CA",
+    "x-ws-profile": "trade",
+    "x-platform-os": "web",
+    "x-ws-device-id": devid,  # "Real" device ID doesn't seem necessary
+})
 
 with connect(
     'wss://realtime-api.wealthsimple.com/subscription',
@@ -38,14 +63,7 @@ with connect(
 ) as ws:
     init = {
         "type": "connection_init",
-        "payload": {
-            "Authorization": auth,
-            "x-ws-api-version": 12,
-            "x-ws-locale": "en-CA",
-            "x-ws-profile": "trade",
-            "x-platform-os": "web",
-            "x-ws-device-id": devid,  # "Real" device ID doesn't seem necessary
-        }
+        "payload": sess_info,
     }
     ws.send(json.dumps(init))
     print(ws.recv())
