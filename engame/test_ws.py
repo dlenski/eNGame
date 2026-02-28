9d62641f9fabf1778d70d2c7c985bcbc9799e107
Daniel Lenski <dlenski@gmail.com>
2026-02-27 21:42:18 -0800

    Comments on WS graphql endpoints

--- engame/test_ws.py
+++ engame/test_ws.py
@@ -17,6 +17,54 @@ from . import ws_auth
 sess = ws_auth._new_ws_session()
 u = ws_auth.authenticate(sess=sess)
 
+# Fetch wealthsimple exchange rate:
+#curl 'https://my.wealthsimple.com/graphql' \
+#  -H 'authorization: Bearer '+jwt \
+#  -H 'content-type: application/json' \
+#  -H 'user-agent: '+ua \
+#  -H 'x-platform-os: web' \
+#  -H 'x-ws-api-version: 12' \
+#  -H 'x-ws-device-id: '+devid \
+#  -H 'x-ws-locale: en-CA' \
+#  -H 'x-ws-profile: trade' \
+#  --data-raw $'{"operationName":"FetchLatestExchangeRate","variables":{"baseCurrency":"USD","quoteCurrency":"CAD"},"query":"query FetchLatestExchangeRate($baseCurrency: ForexCurrency\u0021, $quoteCurrency: ForexCurrency\u0021) {\\n  latestRate(baseCurrency: $baseCurrency, quoteCurrency: $quoteCurrency) {\\n    ...LatestExchangeRate\\n    __typename\\n  }\\n}\\n\\nfragment LatestExchangeRate on ExchangeRate {\\n  mid\\n  bid\\n  ask\\n  __typename\\n}"}'
+
+# Fetch wealthsimple security IDs:
+#curl 'https://my.wealthsimple.com/graphql' \
+#  -H 'authorization: Bearer '+jwt
+#  -H 'content-type: application/json' \
+#  -A ua \
+#  -H 'x-platform-os: web' \
+#  -H 'x-ws-api-version: 12' \
+#  -H 'x-ws-device-id: '+devid \
+#  -H 'x-ws-locale: en-CA' \
+#  -H 'x-ws-profile: trade' \
+#  --data-raw $'{"operationName":"FetchSecuritySearchResult","variables":{"query":"enbri"},"query":"query FetchSecuritySearchResult($query: String\u0021) {\\n  securitySearch(input: {query: $query}) {\\n    results {\\n      ...SecuritySearchResult\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment SecuritySearchResult on Security {\\n  id\\n  buyable\\n  status\\n  stock {\\n    symbol\\n    name\\n    primaryExchange\\n    __typename\\n  }\\n  features\\n  quoteV2 {\\n    securityId\\n    currency\\n    ... on EquityQuote {\\n      marketStatus\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}"}'
+
+# Fetch wealthsimple quote:
+# curl 'https://my.wealthsimple.com/graphql' \
+#  -H 'authorization: Bearer '+jwt
+#  -H 'content-type: application/json' \
+#  -H ua \
+#  -H 'x-platform-os: web' \
+#  -H 'x-ws-api-version: 12' \
+#  -H 'x-ws-device-id: '+devid \
+#  -H 'x-ws-locale: en-CA' \
+#  -H 'x-ws-profile: trade' \
+#  --data-raw $'{"operationName":"FetchSecurityQuoteV2","variables":{"currency":null,"id":"sec-s-d670c0c7745743f8a0f469b2f02444fc"},"query":"query FetchSecurityQuoteV2($id: ID\u0021, $currency: Currency = null) {\\n  security(id: $id) {\\n    id\\n    quoteV2(currency: $currency) {\\n      ...SecurityQuoteV2\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment StreamedSecurityQuoteV2 on UnifiedQuote {\\n  __typename\\n  securityId\\n  ask\\n  bid\\n  currency\\n  price\\n  sessionPrice\\n  quotedAsOf\\n  ... on EquityQuote {\\n    marketStatus\\n    askSize\\n    bidSize\\n    close\\n    high\\n    last\\n    lastSize\\n    low\\n    open\\n    mid\\n    volume: vol\\n    referenceClose\\n    __typename\\n  }\\n  ... on OptionQuote {\\n    marketStatus\\n    askSize\\n    bidSize\\n    close\\n    high\\n    last\\n    lastSize\\n    low\\n    open\\n    mid\\n    volume: vol\\n    breakEven\\n    inTheMoney\\n    liquidityStatus\\n    openInterest\\n    underlyingSpot\\n    __typename\\n  }\\n}\\n\\nfragment SecurityQuoteV2 on UnifiedQuote {\\n  ...StreamedSecurityQuoteV2\\n  previousBaseline\\n  __typename\\n}"}'
+
+if False:
+    p0 = pairs.ng_pairs[0]
+    r = sess.post('https://my.wealthsimple.com/graphql',
+        json=json.loads('{"operationName":"FetchSecurityQuoteV2","variables":{"currency":null,"id":"'+p0.wssecid_cad+'"},"query":"query FetchSecurityQuoteV2($id: ID\u0021, $currency: Currency = null) {\\n  security(id: $id) {\\n    id\\n    quoteV2(currency: $currency) {\\n      ...SecurityQuoteV2\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment StreamedSecurityQuoteV2 on UnifiedQuote {\\n  __typename\\n  securityId\\n  ask\\n  bid\\n  currency\\n  price\\n  sessionPrice\\n  quotedAsOf\\n  ... on EquityQuote {\\n    marketStatus\\n    askSize\\n    bidSize\\n    close\\n    high\\n    last\\n    lastSize\\n    low\\n    open\\n    mid\\n    volume: vol\\n    referenceClose\\n    __typename\\n  }\\n  ... on OptionQuote {\\n    marketStatus\\n    askSize\\n    bidSize\\n    close\\n    high\\n    last\\n    lastSize\\n    low\\n    open\\n    mid\\n    volume: vol\\n    breakEven\\n    inTheMoney\\n    liquidityStatus\\n    openInterest\\n    underlyingSpot\\n    __typename\\n  }\\n}\\n\\nfragment SecurityQuoteV2 on UnifiedQuote {\\n  ...StreamedSecurityQuoteV2\\n  previousBaseline\\n  __typename\\n}"}'),
+        headers={'Referer': f'https://my.wealthsimple.com/app/security-details/{p0.wssecid_cad}',
+                 'Authorization': 'Bearer ' + u.access_token}
+    )
+    r.raise_for_status()
+    from pprint import pprint
+    print(p0.cad)
+    pprint(r.json())
+
 sess_info = {
     "authorization": 'Bearer ' + u.access_token,
     "x-ws-api-version": '12',
@@ -60,6 +108,7 @@ with connect(
 
     for msg in ws:
         j = json.loads(msg)
+        #print('*****', msg)
         jq = j['payload']['data']['securityQuoteUpdates']['quoteV2']
         sym = ws2sym[jq['securityId']]
         ts = navs(jq, 'quotedAsOf', converter=datetime.fromisoformat)
